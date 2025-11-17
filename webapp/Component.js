ddda9bb2588157c03f20b58bee4264fb096a0a68
Timo Fischer <timo.fischer@blum.com>
2025-11-17 07:44:50 +0100

    Step7

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	'sap/ui/core/UIComponent',
-	'sap/ui/model/json/JSONModel'
-], function(UIComponent, JSONModel) {
+	'sap/ui/model/json/JSONModel',
+	'sap/f/library'
+], function(UIComponent, JSONModel, fioriLibrary) {
 	'use strict';
 
 	return UIComponent.extend('sap.ui.demo.fcl.Component', {
@@ -11,14 +12,36 @@ sap.ui.define([
 		},
 
 		init: function () {
-			var oProductsModel;
+			var oModel,
+				oProductsModel,
+				oRouter;
 
 			UIComponent.prototype.init.apply(this, arguments);
 
-			// set products demo model on this sample
-			oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock/products.json'));
-			oProductsModel.setSizeLimit(1000);
-			this.setModel(oProductsModel, 'products');
+			oModel = new JSONModel();
+			this.setModel(oModel);
+
+			// --- Products model (your original data) ---
+            oProductsModel = new JSONModel(sap.ui.require.toUrl('sap/ui/demo/mock/products.json'));
+            oProductsModel.setSizeLimit(1000);
+            this.setModel(oProductsModel, 'products');
+			// ------------------------------
+
+			oRouter = this.getRouter();
+			oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
+			oRouter.initialize();
+		},
+
+		_onBeforeRouteMatched: function(oEvent) {
+			var oModel = this.getModel(),
+				sLayout = oEvent.getParameters().arguments.layout;
+
+			// If there is no layout parameter, set a default layout (normally OneColumn)
+			if (!sLayout) {
+				sLayout = fioriLibrary.LayoutType.OneColumn;
+			}
+
+			oModel.setProperty("/layout", sLayout);
 		}
 	});
 });
